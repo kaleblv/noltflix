0cc47068c1bd1a98d10284b8219e3f9070767310
Kaleb <escobar97@hotmail.com>
2017-07-02 22:24:24 -0500

    added mobile search input

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchForm from './SearchForm';
+import SearchFormMb from './SearchFormMb';
 import logo from '../images/noltflix-logo.svg';
 import iconUser from '../images/icon-user.svg';
 import iconDownArrow from '../images/icon-down-arrow.svg';
@@ -40,15 +41,15 @@ class Header extends React.Component {
           <div className="header-content-mb">
             <div className="user">
               <p className="user__menu-wrapper">
-                <a href="" className="user__menu-toggle">
+                <a href="" className="user__menu-toggle user__menu-toggle-mb">
                   <img src={iconUser} className="user__icon" alt=""/>
                   <span className="user__name">John Smith</span>
                   <img src={iconDownArrow} className="user__down-arrow" alt=""/>
                 </a>
               </p>
             </div>
-            <a href="/" className="header-logo"><img src={logo} className="noltflix_logo" alt=""/></a>
-            <SearchForm/>
+            <a href="/" className="header-logo"><img src={logo} className="noltflix_logo noltflix_logo-mb" alt=""/></a>
+            <SearchFormMb/>
           </div>
         </div>
       </header>)
---
+++ src/components/SearchFormMb.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import iconSearch from '../images/icon-search.svg';
+
+
+
+class SearchFormMb extends React.Component {
+
+  constructor() {
+    super();
+    this.goToDetail = this.goToDetail.bind(this);
+
+    this.state = {
+      search: {}
+    }
+  }
+
+
+  goToDetail(event) {
+
+    event.preventDefault();
+
+    fetch('https://netflixroulette.net/api/api.php?title=' + this.searchInput.value )
+    .then((response) => {
+        return response.json()
+    })
+    .then((search) => {
+
+        this.setState({ search });
+
+        if (this.state.search.show_title) {
+          localStorage.setItem(`detail-item`,JSON.stringify(this.state.search));
+          this.context.router.transitionTo('/detail/' + this.state.search.show_title)
+        }
+    })
+
+  }
+
+
+
+  showInput () {
+    const searchBtn = document.getElementsByClassName('search__submit-btn-mb');
+    const searchInput = document.getElementsByClassName('search__input-mb');
+    const logo = document.getElementsByClassName('noltflix_logo-mb');
+    const user = document.getElementsByClassName('user__menu-toggle-mb');
+    searchInput[0].classList.add("show");
+    searchBtn[0].classList.add("show");    
+    logo[0].classList.add("hide");
+    user[0].classList.add("hide");
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <form className="search__form" onSubmit={this.goToDetail}>
+          <input className="search__input search__input-mb" type="text" name="" defaultValue="" ref={(input) => {
+            this.searchInput = input
+          }} placeholder="Search Media"/>
+          <button type="submit" className="search__submit-btn search__submit-btn-mb">
+            <img src={iconSearch} className="icon-search icon-search-mb" alt=""/>
+          </button>
+          <span className="search__icon" onClick={this.showInput}><img src={iconSearch} className="icon-search-mb" alt=""/></span>
+        </form>
+      </div>
+    )
+  }
+}
+
+SearchFormMb.contextTypes = {
+  router: React.PropTypes.object
+}
+
+export default SearchFormMb;
